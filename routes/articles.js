ac7936e81344cbe1c98c985169c57332ecf83008
Aung Han <doubleaung5@gmail.com>
2023-09-17 12:57:48 -0300

    created new article page

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.send('this is article')
+    res.render('articles/new')
 })
 
 module.exports = router
