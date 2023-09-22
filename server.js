71aac77972f16b88c5bcafabe083a021b340b5e8
Aung Han <doubleaung5@gmail.com>
2023-09-22 11:21:48 -0300

    added delete route and function

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/articles')
 const articlesRouter = require('./routes/articles')
+const methodOverride = require('method-override')  //method-override middleware
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/markdownBlog')
 
 app.set('view engine', 'ejs');    //  to use EJS template engine
 app.use(express.urlencoded({ extended: false })) // to access the input from article form 
+app.use(methodOverride('_method')) // "_method" to override the standard GET and POST methods.
+// to use PUT and DELETE HTTP methods for actions like updating and deleting resources within web applications, even when HTML forms primarily support GET and POST methods.
+
 app.use('/articles', articlesRouter) // "/articles" will be routed to the articlesRouter
 
 
