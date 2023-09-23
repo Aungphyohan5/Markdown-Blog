00db77644495ffd97ec88528a8861e99007d316c
Aung Han <doubleaung5@gmail.com>
2023-09-23 19:42:44 -0300

    completed slugify and marked

--- models/articles.js
+++ models/articles.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose')
-const marked = require('marked')  // convert text to HTML
+const marked = require('marked'); // convert text to HTML..used v3.0.7
 const slugify = require('slugify') // URL friendly ( to change ugly id to title name)
+const createDomPurify = require('dompurify')  // used to sanitize and clean HTML content, removing potentially unsafe or malicious code
+const { JSDOM } = require('jsdom')  // for dompurify
+const dompurify = createDomPurify(new JSDOM().window) // to clean and make HTML content safe
+
 const articleSchema = new mongoose.Schema({
     title: {
         type: String,
-        require: true
+        required: true
     },
     description: {
         type: String,
     },
     markdown: {
         type: String,
-        require: true
+        required: true
     },
     createdAt: {
         type: Date,
@@ -19,8 +23,12 @@ const articleSchema = new mongoose.Schema({
     },
     slug: {
         type: String,
-        require: true,
+        required: true,
         unique: true
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
     }
 })
 // Run this function before CRUD - it will set title name for slug
@@ -31,7 +39,14 @@ articleSchema.pre('validate', function (next) {
     if (this.title) {
         this.slug = slugify(this.title, { lower: true, strict: true })
     }
+    if (this.markdown) {
+        // marked(this.markdown) - create html
+        // dompurify.sanitize - santize the html
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    }
     next()
 })
+
+
 
 module.exports = mongoose.model('article', articleSchema)
